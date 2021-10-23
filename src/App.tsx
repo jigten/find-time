78443925d7d46a3cec554134dc17c91dbd08e08f
Ugyen Jigten <ujigten@gmail.com>
2021-10-23 11:37:43 +0600

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+
+const App: React.FC<{}> = () => {
+	return (
+		<>
+			<HelloWorld />
+		</>
+	);
+};
+
+export default App;
